7e8dc1369d9ddb7269409a69ea2e0e2d2fbf34a3
Jas <contact@jaslamba.me>
2018-05-07 15:29:21 -0400

    setup live database

--- application/config/database.php
+++ application/config/database.php
@@ -94,3 +94,26 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => '107.180.32.184',
+// 	// 'username' => 'root',
+// 	'username' => 'jas',
+// 	'password' => 'May072018',
+// 	'database' => 'certus-live',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
