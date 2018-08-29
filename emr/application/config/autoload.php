7fe3813c22d85badee02a784d4dae1f4ff249da4
Jas <contact@jaslamba.me>
2018-08-29 17:00:16 -0400

    login done

--- emr/application/config/autoload.php
+++ emr/application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('session', 'form_validation');
+$autoload['libraries'] = array('database', 'session', 'form_validation', 'ion_auth');
 
 /*
 | -------------------------------------------------------------------
@@ -79,7 +79,7 @@ $autoload['libraries'] = array('session', 'form_validation');
 |	$autoload['drivers'] = array('cache' => 'cch');
 |
 */
-$autoload['drivers'] = array('session');
+$autoload['drivers'] = array();
 
 /*
 | -------------------------------------------------------------------
