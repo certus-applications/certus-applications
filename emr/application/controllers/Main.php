d191f1d0bb2e94035a2bad42c0e218c7018071cb
Jas <contact@jaslamba.me>
2018-08-29 15:23:04 -0400

    something went wrong...

--- emr/application/controllers/Main.php
+++ emr/application/controllers/Main.php
@@ -4,6 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Main extends CI_Controller {
     public function __construct() {
       parent::__construct();
+
+      if (!$this->ion_auth->logged_in()) {
+        redirect('auth/login', 'refresh');
+      }
     }
 
     public function index(){
