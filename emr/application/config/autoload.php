80d979fac982f9a08e65f1ae613a82501f68f545
Jas <contact@jaslamba.me>
2018-08-29 15:49:49 -0400

    I fucked up...

--- emr/application/config/autoload.php
+++ emr/application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('session', 'form_validation', 'ion_auth');
+$autoload['libraries'] = array('session', 'form_validation');
 
 /*
 | -------------------------------------------------------------------
@@ -117,7 +117,7 @@ $autoload['config'] = array();
 | "codeigniter_lang.php" would be referenced as array('codeigniter');
 |
 */
-$autoload['language'] = array('auth','ion_auth');
+$autoload['language'] = array();
 
 /*
 | -------------------------------------------------------------------
