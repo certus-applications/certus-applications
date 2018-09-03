301624fb5466ca23f859e42888b4d918f523168d
Sandeep Suri <sandeep.suri@ryerson.ca>
2018-09-03 14:06:46 -0400

    main_login look
    got the bootstrap working, now need to get the login page to work

--- emr/application/views/clients/list.php
+++ emr/application/views/clients/list.php
@@ -56,7 +56,7 @@
               <ul class="nav navbar-nav navbar-right">
                 <li class="">
                   <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                    <!-- <img src="images/img.jpg" alt=""> -->Dr. Bains
+                    <!-- <img src="images/img.jpg" alt=""> -->Dr. Manan Patel, PhPiece
                     <span class=" fa fa-angle-down"></span>
                   </a>
                   <ul class="dropdown-menu dropdown-usermenu pull-right">
--- emr/application/views/main_login/index.php
+++ emr/application/views/main_login/index.php
@@ -6,18 +6,18 @@
 <!--===============================================================================================-->	
 	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../../../vendor_login/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../../../vendor_login/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="../../../vendor_login/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../../../vendor_login/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../../../css_login/util.css">
-	<link rel="stylesheet" type="text/css" href="../../../css_login/main.css">
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 <!--===============================================================================================-->
 </head>
 <body>
@@ -26,7 +26,7 @@
 		<div class="container-login100">
 			<div class="wrap-login100">
 				<div class="login100-pic js-tilt" data-tilt>
-					<img src="../../../img/certus_logo.png" alt="IMG">
+					<img src="img/certus_logo.png" alt="IMG">
 				</div>
 
 				<form class="login100-form validate-form">
@@ -80,21 +80,21 @@
 
 	
 <!--===============================================================================================-->	
-	<script src="../../../vendor_login/jquery/jquery-3.2.1.min.js"></script>
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
-	<script src="../../../vendor_login/bootstrap/js/popper.js"></script>
-	<script src="../../../vendor_login/bootstrap/js/bootstrap.min.js"></script>
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
-	<script src="../../../vendor_login/select2/select2.min.js"></script>
+	<script src="vendor/select2/select2.min.js"></script>
 <!--===============================================================================================-->
-	<script src="../../../vendor_login/tilt/tilt.jquery.min.js"></script>
+	<script src="vendor/tilt/tilt.jquery.min.js"></script>
 	<script >
 		$('.js-tilt').tilt({
 			scale: 1.1
 		})
 	</script>
 <!--===============================================================================================-->
-	<script src="../../../js_login/main.js"></script>
+	<script src="js/main.js"></script>
 
 </body>
 </html>
