f18b986c107372c887ebc7a0afc12090a6a5c394
Sandeep Suri <sandeep.suri@ryerson.ca>
2018-09-06 17:54:08 -0400

    clients.sql updated

--- emr/sql/certus.sql
+++ emr/sql/certus.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.8.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Aug 30, 2018 at 04:10 PM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 5.6.34
+-- Host: 127.0.0.1
+-- Generation Time: Sep 06, 2018 at 11:53 PM
+-- Server version: 5.7.19
+-- PHP Version: 7.0.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,32 @@ SET time_zone = "+00:00";
 --
 -- Database: `certus`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+CREATE TABLE `clients` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(256) DEFAULT NULL,
+  `last_name` varchar(256) DEFAULT NULL,
+  `last_visit` date DEFAULT NULL,
+  `name_clinician` varchar(256) DEFAULT NULL,
+  `phone` varchar(256) DEFAULT NULL,
+  `email` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` (`id`, `first_name`, `last_name`, `last_visit`, `name_clinician`, `phone`, `email`) VALUES
+(1, 'John', 'Smith', '2018-08-06', 'Dr. Bains', '9055505500', 'john.smith@gmail.com'),
+(2, 'Sam', 'Smith', '2018-09-03', 'Dr. Manan Patel', '6311234090', 'sam.smith@ex.ca'),
+(3, 'Ted', 'Rogers', '2018-07-31', 'Dr. Indianna', '1234567890', 'rogers@trs.ca'),
+(4, 'Manan', 'Patel', '2018-08-23', 'Dr. Vaibhev Sharma', '6471231234', 'manu@manu.ca');
 
 -- --------------------------------------------------------
 
@@ -87,8 +113,10 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `salt`, `email`, `activation_code`, `forgotten_password_code`, `forgotten_password_time`, `remember_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `company`, `phone`) VALUES
 (1, '127.0.0.1', 'administrator', '$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36', '', 'admin@admin.com', '', NULL, NULL, NULL, 1268889823, 1535638008, 1, 'Admin', 'istrator', 'ADMIN', '0'),
-(2, '127.0.0.1', 'jas.lamba@certusapplication.ca', '$2y$08$LJqNblxnGFGW05wNtk9mJu8yMwkJD0SXYjLnvPIIFtAh5YxXDAps2', NULL, 'jas.lamba@certusapplication.ca', NULL, NULL, NULL, NULL, 1535638078, 1535638206, 1, 'Jaskiran', 'Lamba', 'Certus', '6474096157'),
-(3, '127.0.0.1', 'sandeep.suri@certusapplication.ca', '$2y$08$AZL5SeVKoMJZLrhnZHQskO.hGwyvFdvptOrUW9gjlOIIuYMp0v/aa', NULL, 'sandeep.suri@certusapplication.ca', NULL, NULL, NULL, NULL, 1535638158, NULL, 1, 'Sandeep', 'Suri', 'Certus', '‭6477840040‬');
+(2, '127.0.0.1', 'jas.lamba@certusapplication.ca', '$2y$08$LJqNblxnGFGW05wNtk9mJu8yMwkJD0SXYjLnvPIIFtAh5YxXDAps2', NULL, 'jas.lamba@certusapplication.ca', NULL, NULL, NULL, NULL, 1535638078, 1536098136, 1, 'Jaskiran', 'Lamba', 'Certus', '6474096157'),
+(3, '127.0.0.1', 'sandeep.suri@certusapplication.ca', '$2y$08$q2Y1YdroiS/I/dBNk9tGxOioWrfBHSiCMrCNuee042wKmtlGmE02u', NULL, 'sandeep.suri@certusapplication.ca', NULL, NULL, NULL, NULL, 1535638158, NULL, 1, 'Sandeep', 'Suri', 'Certus', '‭6477840040‬'),
+(4, '127.0.0.1', 'sunny.suri@gmail.com', '$2y$08$S5ktQcv2GeqHhvbXqmsSxeKhoAc4055UVkB66hzRsMvlYNKJfp9Zm', NULL, 'sunny.suri@gmail.com', NULL, NULL, NULL, NULL, 1535670538, 1536270045, 1, 'Sunny', 'Suri', 'Certus', '123456'),
+(5, '127.0.0.1', 'test@test.ca', '$2y$08$56AjCucwexNqraFyR.Jy9eIj4YMTB97lddC7xsYWe9BrZ.o8N.7ry', NULL, 'test@test.ca', NULL, NULL, NULL, NULL, 1536098199, 1536099459, 1, 'test', 'test', 'test', '123456');
 
 -- --------------------------------------------------------
 
@@ -111,12 +139,21 @@ INSERT INTO `users_groups` (`id`, `user_id`, `group_id`) VALUES
 (2, 1, 2),
 (4, 2, 1),
 (5, 2, 2),
-(7, 3, 1),
-(8, 3, 2);
+(11, 3, 1),
+(12, 3, 2),
+(14, 4, 1),
+(15, 4, 2),
+(16, 5, 2);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `groups`
@@ -150,6 +187,12 @@ ALTER TABLE `users_groups`
 --
 
 --
+-- AUTO_INCREMENT for table `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
@@ -159,19 +202,19 @@ ALTER TABLE `groups`
 -- AUTO_INCREMENT for table `login_attempts`
 --
 ALTER TABLE `login_attempts`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users_groups`
 --
 ALTER TABLE `users_groups`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Constraints for dumped tables
