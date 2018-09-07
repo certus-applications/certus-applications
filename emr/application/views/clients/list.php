1c7f9ca62eae679a76506f8d189ac827ea47077d
Jas <contact@jaslamba.me>
2018-09-06 20:03:13 -0400

    deleting client

--- emr/application/views/clients/list.php
+++ emr/application/views/clients/list.php
@@ -35,7 +35,7 @@
                               <td>
                                 <a href="" class="btn btn-primary btn-xs"><i class="fa fa-folder"></i> View </a>
                                 <a href="" class="btn btn-info btn-xs"><i class="fa fa-pencil"></i> Edit </a>
-                                <a href="" class="btn btn-danger btn-xs" data-target="#deleteClient"><i class="fa fa-trash-o"></i> Delete </a>
+                                <a data-toggle="modal" class="btn btn-danger btn-xs" data-target="#deleteClient<?php echo $clients['id'];  ?>"><i class="fa fa-trash-o"></i> Delete </a>
                               </td>
                             </tr>
                           <?php } ?>
@@ -54,3 +54,26 @@
     </div>
 
   </body>
+
+  <!-- Modal -->
+<?php foreach($clientsAll as $clients){?>
+  <div class="modal fade" id="deleteClient<?php echo $clients['id'];  ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <?php echo $clients['email'];  ?>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+<?php } ?>
