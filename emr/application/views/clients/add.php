73f31a8099250c6c95c3a71a5336681baefce7d3
Sandeep Suri <sandeep.suri@ryerson.ca>
2018-09-06 22:37:29 -0400

    Add clients page

--- emr/application/views/clients/add.php
+++ emr/application/views/clients/add.php
@@ -1,1 +1,57 @@
-add.php
+                <!-- page content -->
+        <div class="right_col" role="main">
+                      <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>New Clients <small>Add in new patients here</small></h2>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                      <tbody>
+                          <form method='post'>
+		
+							<fieldset class="form-group">
+								<input class="form-control" type="text" name="first_name" placeholder="First Name ex. Bob">
+							</fieldset> 
+
+							<fieldset class="form-group">
+								<input class="form-control" type="text" name="last_name" placeholder="Last Name ex. Dylan">
+							</fieldset> 
+
+							<fieldset class="form-group">   
+								<input class="form-control" type="date" name="last_visit" placeholder="Last Visit">
+							</fieldset>
+
+							<fieldset class="form-group">   
+								<input class="form-control" type="text" name="clinician" placeholder="Name of Clinician">
+							</fieldset>
+
+							<fieldset class="form-group">   
+								<input class="form-control" type="number" name="telephone" placeholder="Phone Number">
+							</fieldset>
+
+							<fieldset class="form-group">   
+								<input class="form-control" type="email" name="email" placeholder="email ex. bob@example.ca">
+							</fieldset>
+
+							<fieldset class="form-group">       
+								<a href=""><button  type="submit" class="btn btn-success">Add Client</button></a>
+							</fieldset>
+
+					</form>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /page content -->
+
+
+      </div>
+    </div>
+
+  </body>
