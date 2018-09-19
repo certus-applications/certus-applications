6ecd6340932fa9f74e126e151be431598dc678e9
Sandeep Suri <sandeep.suri@ryerson.ca>
2018-09-19 18:25:58 -0400

    login folder
    works on safari

--- emr/application/views/auth/login.php
+++ emr/application/views/auth/login.php
@@ -31,23 +31,31 @@
 
 -->
 
+
+
 <html lang="en">
 <head>
   <title>Certus</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->  
-  <link rel="icon" type="image/png" href="../../../img/icons/rsz_1certuslogowatermark.png"/>
+  <link rel="icon" type="image/png" href="../../../img/rsz_1certuslogowatermark.png"/>
 <!--===============================================================================================-->
-  <link rel="stylesheet" type="text/css" href="../../../vendor_login/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="../../../vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
-  <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="../../../fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
-  <link rel="stylesheet" type="text/css" href="../../../vendor_login/animate/animate.css">
+  <link rel="stylesheet" type="text/css" href="../../../fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="../../../vendor/animate/animate.css">
 <!--===============================================================================================-->  
-  <link rel="stylesheet" type="text/css" href="../../../vendor_login/css-hamburgers/hamburgers.min.css">
+  <link rel="stylesheet" type="text/css" href="../../../vendor/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
-  <link rel="stylesheet" type="text/css" href="../../../vendor_login/select2/select2.min.css">
+  <link rel="stylesheet" type="text/css" href="../../../vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="../../../vendor/select2/select2.min.css">
+<!--===============================================================================================-->  
+  <link rel="stylesheet" type="text/css" href="../../../vendor/daterangepicker/daterangepicker.css">
 <!--===============================================================================================-->
   <link rel="stylesheet" type="text/css" href="../../../css_login/util.css">
   <link rel="stylesheet" type="text/css" href="../../../css_login/main.css">
@@ -55,55 +63,62 @@
 </head>
 <body>
 
+  
   <div class="limiter">
-    <div class="container-login100">
-      <div class="wrap-login100">
-        <div class="login100-pic js-tilt" data-tilt>
-          <img src="../../../img/certus_logo.png" alt="IMG">
-        </div>
+    <div class="container-login100" style="background-image: url('../../../img/bg-02.jpg');">
+      <div class="wrap-login100 p-l-110 p-r-110 p-t-62 p-b-33">
 
         <?php echo form_open("auth/login");?>
-        <form class="login100-form validate-form">
-          <span class="login100-form-title">
-            Member Login
+        <form class="login100-form validate-form flex-sb flex-w">
+          <span class="login100-form-title p-b-53">
+            <h1><?php echo lang('login_heading');?></h1>
+            <p><?php echo lang('login_subheading');?></p>
           </span>
 
-       
-          <?php echo form_open("auth/login");?>
-          <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-            <?php echo lang('login_identity_label', 'identity');?>
-              <?php echo form_input($identity);?>
-            <span class="focus-input100"></span>
-            <span class="symbol-input100">
-              <i class="fa fa-envelope" aria-hidden="true"></i>
+
+          <!-- Username -->
+          <div class="p-t-31 p-b-9">
+            <span class="txt1">
+              <?php echo lang('login_identity_label', 'identity');?>
             </span>
           </div>
-
-          <div class="wrap-input100 validate-input" data-validate = "Password is required">
-            <?php echo lang('login_password_label', 'password');?>
-              <?php echo form_input($password);?>
+          <div class="wrap-input100 validate-input" data-validate = "Username is required" style='text-align: center; background-color: white; border: solid 4px grey;border-radius: 20px;'>
+            <?php echo form_input($identity);?>
             <span class="focus-input100"></span>
-            <span class="symbol-input100">
-              <i class="fa fa-lock" aria-hidden="true"></i>
+          </div>
+
+
+          <!-- Password -->
+          <div class="p-t-13 p-b-9">
+            <span class="txt1">
+              <?php echo lang('login_password_label', 'password');?>
             </span>
           </div>
-
-          <div class="wrap-input100">
-            <?php echo lang('login_remember_label', 'remember');?>
-            <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
+          <div class="wrap-input100 validate-input" data-validate = "Password is required" style='text-align: center; background-color: white; border: solid 4px grey;border-radius: 20px;'>
+            <?php echo form_input($password);?>
+            <span class="focus-input100"></span>
           </div>
-          
-          <div class="container-login100-form-btn">
+
+          <div class="container-login100-form-btn m-t-17">
             <button class="login100-form-btn">
-              Login
+              Sign In
               <?php echo form_submit('submit');?>
             </button>
           </div>
 
-          <div class="text-center p-t-12">
-            <a class="txt2" href="forogt_password">
-              Forgot your password?
+          <div class="w-full text-center p-t-55">
+
+            <p><a href="forgot_password"><?php echo lang('login_forgot_password');?></a></p>
+
+            <br>
+
+            <span class="txt2">
+              Not a member?
+            </span>
+            <a href="#" class="txt2 bo1">
+              Sign up now
             </a>
+
           </div>
         </form>
         <?php echo form_close();?>
@@ -112,23 +127,22 @@
   </div>
   
 
+  <div id="dropDownSelect1"></div>
   
-
-  
-<!--===============================================================================================-->  
-  <script src="../../../vendor_login/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
-  <script src="../../../vendor_login/bootstrap/js/popper.js"></script>
-  <script src="../../../vendor_login/bootstrap/js/bootstrap.min.js"></script>
+  <script src="../../../vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
-  <script src="../../../vendor_login/select2/select2.min.js"></script>
+  <script src="../../../vendor/animsition/js/animsition.min.js"></script>
 <!--===============================================================================================-->
-  <script src="../../../vendor_login/tilt/tilt.jquery.min.js"></script>
-  <script >
-    $('.js-tilt').tilt({
-      scale: 1.1
-    })
-  </script>
+  <script src="../../../vendor/bootstrap/js/popper.js"></script>
+  <script src="../../../vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+  <script src="../../../vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+  <script src="../../../vendor/daterangepicker/moment.min.js"></script>
+  <script src="../../../vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+  <script src="../../../vendor/countdowntime/countdowntime.js"></script>
 <!--===============================================================================================-->
   <script src="../../../js_login/main.js"></script>
 
