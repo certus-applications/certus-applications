83af3b101b8db2b15b8bece6befa82a2850420be
Jas Lamba <jaskiran.lamba@gmail.com>
2021-01-22 16:24:26 -0500

    turning on debugging logs

---
+++ emr/application/logs/log-2021-01-22.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-01-22 17:13:22 --> Severity: error --> Exception: sizeof(): Argument #1 ($var) must be of type Countable|array, null given /Applications/XAMPP/xamppfiles/htdocs/certus/emr/application/controllers/Clients.php 146
+ERROR - 2021-01-22 17:26:31 --> Severity: error --> Exception: sizeof(): Argument #1 ($var) must be of type Countable|array, null given /Applications/XAMPP/xamppfiles/htdocs/certus/emr/application/controllers/Clients.php 146
