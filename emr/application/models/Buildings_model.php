afd9784cdf4cf8aed595238b26b7b81ec81c9ece
Jas Lamba <jaskiran.lamba@gmail.com>
2021-02-18 10:46:15 -0500

    adding calls to be able to read and write data to buildings SQL table

---
+++ emr/application/models/Buildings_model.php
@@ -0,0 +1,58 @@
+<?php
+   // do all database calls here
+   class Buildings_model extends CI_Model {
+
+      function __construct() {
+         parent::__construct();
+      }
+
+      public function listAll(){
+      	$this->db->select('*');
+      	$this->db->from('buildings');
+      	$query=$this->db->get();
+        $returnData = $query->result_array();
+      	return $returnData;
+      }
+
+      public function viewClient($id){
+        $this->db->select('*');
+        $this->db->from('clients');
+        $this->db->where('id', $id);
+        $query = $this->db->get();
+        return $query->result_array(); 
+
+      }
+
+      public function email_check($email){
+
+        $this->db->select('*');
+        $this->db->from('users');
+        $this->db->where('email',$email);
+        $query=$this->db->get();
+
+        if($query->num_rows()>0){
+           return false;
+        } else {
+           return true;
+        }
+      }
+
+      public function login_user($username, $password){
+        $this->db->select('*');
+        $this->db->from('users');
+        $this->db->where('username',$username);
+        $this->db->where('password',$password);
+
+        if($query=$this->db->get()){
+          return $query->row_array();
+        }else{
+          return false;
+        }
+      }
+
+      public function editSchedule($scheduleData){
+        extract($scheduleData);
+        $this->db->where('id', $id);
+        $this->db->update('schedule', $scheduleData);
+      }
+    }
