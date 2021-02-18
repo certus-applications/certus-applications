0e8008823154776bf9595f0c35771bb6c9e9cb16
Jas Lamba <jaskiran.lamba@gmail.com>
2021-02-18 10:45:33 -0500

    UI for adding a new building

---
+++ emr/application/views/buildings/add.php
@@ -0,0 +1,37 @@
+<div class="right_col" role="main">
+<div class="">
+	<div class="clearfix"></div>
+	<div class="row">
+		<div class="col-md-12 col-sm-12 ">
+			<div class="x_panel">
+				<div class="x_title">
+					<h2>Add Building</h2>
+					<div class="clearfix"></div>
+				</div>
+				<div class="x_content">
+					<br />
+					<form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
+
+						<div class="item form-group">
+							<label class="col-form-label col-md-3 col-sm-3 label-align" for="first-name">Name <span class="required">*</span>
+							</label>
+							<div class="col-md-6 col-sm-6 ">
+								<input type="text" id="first-name" required="required" class="form-control ">
+							</div>
+						</div>
+						<div class="ln_solid"></div>
+						<div class="item form-group">
+							<div class="col-md-6 col-sm-6 offset-md-3">
+								<a href="Buildings/index"><button class="btn btn-primary" type="button">Cancel</button></a>
+								<button class="btn btn-primary" type="reset">Reset</button>
+								<button type="submit" class="btn btn-success">Submit</button>
+							</div>
+						</div>
+
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	</div>
+</div>
