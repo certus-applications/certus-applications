2a7550d018bea682f803e06c736e9e4123ae309e
Jas Lamba <jaskiran.lamba@gmail.com>
2021-03-06 13:42:23 -0500

    automatically filtering for today

--- emr/build/js/availability.js
+++ emr/build/js/availability.js
@@ -2,7 +2,43 @@ availability_morning()
 availability_evening()
 availability_night()
 
+document.getElementById("filterby").innerHTML = "Filtered by " + get_today();
+
+$("#dayofweek li").click(function() {
+    var filteredDay
+    filteredDay = this.id;
+    document.getElementById("filterby").innerHTML = "Filtered by " + filteredDay;
+});
+
+function get_today() {
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    var today = new Date();
+    var day = String(today.getDay());
+
+    return weekday[day];
+}
+
 function availability_morning() {
+    ul = document.getElementById("availability_morning");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.indexOf(get_today()) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+
 	$("#dayofweek li").click(function() {
         var filteredDay, ul, li, txtValue
         filteredDay = this.id;
@@ -41,6 +77,18 @@ function availability_morning() {
 }
 
 function availability_evening() {
+    ul = document.getElementById("availability_evening");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.indexOf(get_today()) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+
     $("#dayofweek li").click(function() {
         var filteredDay, ul, li, txtValue
         filteredDay = this.id;
@@ -62,6 +110,18 @@ function availability_evening() {
 
 
 function availability_night() {
+    ul = document.getElementById("availability_night");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.indexOf(get_today()) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+
     $("#dayofweek li").click(function() {
         var filteredDay, ul, li, txtValue
         filteredDay = this.id;
