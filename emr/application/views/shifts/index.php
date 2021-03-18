9d769112fdd06fd19415960d839655d17a86d8d1
Jas Lamba <jaskiran.lamba@gmail.com>
2021-03-18 11:58:25 -0400

    some styling cleanup

--- emr/application/views/shifts/index.php
+++ emr/application/views/shifts/index.php
@@ -115,10 +115,11 @@
 		                        <div id="step-2">
 		                            <form class="form-horizontal form-label-left">
 		                                <div class="form-group row">
-		                                	<!-- <div class="col-md-6 col-sm-3"></div> -->
-		                                    <div class="col-md-12 col-sm-12">
+		                                	<div class="col-md-5 col-sm-3"></div>
+		                                    <div class="col-md-6 col-sm-12">
                                                 <div class="d-flex justify-content-center">
                                                     <input type="checkbox" name="shift" value="checkin"> Check-In
+                                                    <input type="checkbox" name="shift" value="checkin"> Check-Out
                                                 </div>
 		                                    </div>
 		                                </div>
@@ -128,8 +129,8 @@
 		                        <div id="step-3">
 		                            <form class="form-horizontal form-label-left">
 		                                <div class="form-group row">
-		                                	<div class="col-md-5 col-sm-5"></div>
-		                                    <div class="col-md-6 col-sm-6">
+		                                	<div class="col-md-6 col-sm-5"></div>
+		                                    <div class="col-md-5 col-sm-6">
 		                                    	<div style="font-size: 35px;">&#9989;</div>
 		                                    </div>
 		                                </div>
