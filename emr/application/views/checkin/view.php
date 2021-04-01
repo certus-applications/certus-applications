883dc4698ab48e9479486f00d393819ea7ac300d
Sandeep Suri <sandeep.suri@ryerson.ca>
2021-04-01 12:10:50 -0400

    checkin controller and view

---
+++ emr/application/views/checkin/view.php
@@ -0,0 +1,42 @@
+                <!-- page content -->
+                <div class="right_col" role="main">
+                      <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Check-in/Check-out</h2>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                     <table id="datatable" class="table table-striped table-bordered">
+                      <thead>
+                        <tr>
+                          <th>ID</th>
+                          <th>Name</th>
+                          <th>Check-in Time</th>
+                          <th>Check-out Time</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                            <tr>
+                              <td>x1234d</td>
+                              <td>Bob Dylan</td>
+                              <td>Thursday, March 3rd 8:00 am</td>
+                              <td>Thursday, March 3rd 2:00 pm</td>
+                            </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /page content -->
+
+
+      </div>
+    </div>
+
+  </body>
+
